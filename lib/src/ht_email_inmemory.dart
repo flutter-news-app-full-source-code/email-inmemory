568231d7c147aabbbb2b6356417b1d3ccc2d18df
fulleni <fulleni@hotmail.com>
2025-05-04 08:39:43 +0100

    chore: initial commit
    - Added basic project structure
    - Added CI workflow
    - Added issue templates

---
+++ lib/src/ht_email_inmemory.dart
@@ -0,0 +1,7 @@
+/// {@template ht_email_inmemory}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtEmailInmemory {
+  /// {@macro ht_email_inmemory}
+  const HtEmailInmemory();
+}
---
+++ test/src/ht_email_inmemory_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:ht_email_inmemory/ht_email_inmemory.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('HtEmailInmemory', () {
+    test('can be instantiated', () {
+      expect(HtEmailInmemory(), isNotNull);
+    });
+  });
+}
